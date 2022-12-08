8c04a05510e4721893b0aabe02bc61ff49ef06f7
aidilrx04 <mrcrayv14@gmail.com>
2022-12-08 16:15:28 +0800

    [+] add fetch data from server on page load

---
+++ src/routes/game/+page.ts
@@ -0,0 +1,38 @@
+import { MODE_TYPES, type Mode } from '$lib/modes';
+import { redirect } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async (req) => {
+	const { url } = req;
+
+	const modeStr = url.searchParams.get('mode');
+	const mode = getModeFromStr(modeStr ?? '') as Mode;
+	const shouldRedirect = modeStr && !mode;
+
+	if (shouldRedirect) throw redirect(307, '/mode');
+
+	const totalWords = 30;
+	const wordLength = mode.grid.column - 2;
+
+	const apiUrl = 'https://bahasa-api.vercel.app/api/word';
+	const queryString = new URLSearchParams({
+		amount: totalWords.toString(),
+		length: wordLength.toString()
+	}).toString();
+
+	const response = await req.fetch(`${apiUrl}?${queryString}`);
+	const responseData = await response.json();
+
+	return {
+		words: responseData as { id: number; word: string; length: number }[],
+		type: mode
+	};
+};
+
+const getModeFromStr = (modeStr: string): Mode | false => {
+	const validModes = ['easy', 'normal', 'hard'];
+	const modeIndex = validModes.indexOf(modeStr);
+	if (modeIndex < 0) return false;
+
+	return MODE_TYPES[modeIndex + 1];
+};
