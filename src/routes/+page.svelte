3598ca46b03212053f575e34a54ab9e9b2a1abb7
aidilrx04 <mrcrayv14@gmail.com>
2022-12-10 00:57:44 +0800

    add basic homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,27 @@
-<div id="app">
-
-    <a href="/game">
-        Go to Game
-    </a>
-
+<div id="app" class="max-w-[360px] mx-auto">
+	<header class="mx-2 bg-gray-50 py-7 px-3 my-4 rounded-xl">
+		<div class="title">
+			<span class="block text-slate-700">Selamat Datang ke</span>
+			<h1 class="text-5xl text-slate-900 font-semibold">Cari Kata</h1>
+		</div>
+	</header>
+	<main class="p-3 bg-gray-50 mx-2">
+		<section>
+			<div class="flex justify-center items-center">
+				<a
+					href="/mode"
+					class="play-button my-2 bg-violet-600 text-lg uppercase text-center rounded-md shadow-lg text-white px-3 py-2 max-w-full mx-2 hover:bg-violet-700 transition-colors"
+					>MAIN SEKARANG</a
+				>
+			</div>
+		</section>
+		<section class="about my-3 py-2">
+			<h2 class="text-lg text-slate-800">TENTANG</h2>
+			<p class="text-md text-slate-600">
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit voluptatum facilis tenetur
+				molestias quidem, earum numquam placeat incidunt ex aliquam fugiat distinctio unde minus non
+				dignissimos quod laudantium illum sapiente!
+			</p>
+		</section>
+	</main>
 </div>
