6047df5fe98ba05ecc457ae2c1688a3e3293f1f3
aidilrx04 <mrcrayv14@gmail.com>
2022-12-22 17:17:31 +0800

    fix container tag add bg color

--- src/routes/mode/+page.svelte
+++ src/routes/mode/+page.svelte
@@ -7,45 +7,46 @@
 <div class="w-[360px] mx-auto">
 	<header>
 		<Navigation />
+		<div class="title w-[300px] mx-auto">
+			<h2 class="uppercase text-2xl text-center mt-6 mb-3">Mode</h2>
+		</div>
 	</header>
 
-	<div id="mode-selection">
-		<div class="title w-[300px] mx-auto">
-			<h2 class="uppercase text-2xl text-center my-2">Mode</h2>
+	<main id="mode-selection" class="px-3">
+		<div class="container bg-gray-50 rounded-lg max-w-full">
+			<div class="modes-cards mx-auto flex justify-center items-center flex-col">
+				<ModeCard ctaHref="/game?mode=easy">
+					<div class="title" slot="title">Mudah</div>
+					<div slot="description">
+						<ul class="list-disc ml-5">
+							<li>8x9 grid</li>
+							<li>Perkataan terbalik kurang</li>
+							<li>Perkataan condong kurang</li>
+						</ul>
+					</div>
+				</ModeCard>
+				<ModeCard ctaHref="/game?mode=normal">
+					<div class="title" slot="title">Biasa</div>
+					<div slot="description">
+						<ul class="list-disc ml-5">
+							<li>8x9 grid</li>
+							<li>Perkataan terbalik kurang</li>
+							<li>Perkataan condong kurang</li>
+						</ul>
+					</div>
+				</ModeCard>
+				<ModeCard ctaHref="/game?mode=hard">
+					<div class="title" slot="title">Susah</div>
+					<div slot="description">
+						<ul class="list-disc ml-5">
+							<li>8x9 grid</li>
+							<li>Perkataan terbalik kurang</li>
+							<li>Perkataan condong kurang</li>
+						</ul>
+					</div>
+				</ModeCard>
+			</div>
 		</div>
-
-		<div class="modes-cards mx-auto w-[300px] flex justify-center items-center flex-col">
-			<ModeCard ctaHref="/game?mode=easy">
-				<div class="title" slot="title">Mudah</div>
-				<div slot="description">
-					<ul class="list-disc ml-5">
-						<li>8x9 grid</li>
-						<li>Perkataan terbalik kurang</li>
-						<li>Perkataan condong kurang</li>
-					</ul>
-				</div>
-			</ModeCard>
-			<ModeCard ctaHref="/game?mode=normal">
-				<div class="title" slot="title">Biasa</div>
-				<div slot="description">
-					<ul class="list-disc ml-5">
-						<li>8x9 grid</li>
-						<li>Perkataan terbalik kurang</li>
-						<li>Perkataan condong kurang</li>
-					</ul>
-				</div>
-			</ModeCard>
-			<ModeCard ctaHref="/game?mode=hard">
-				<div class="title" slot="title">Susah</div>
-				<div slot="description">
-					<ul class="list-disc ml-5">
-						<li>8x9 grid</li>
-						<li>Perkataan terbalik kurang</li>
-						<li>Perkataan condong kurang</li>
-					</ul>
-				</div>
-			</ModeCard>
-		</div>
-	</div>
+	</main>
 	<Footer />
 </div>
