a565d42af040a96ff581f57a2355b1f232dfe2fd
aidilrx04 <mrcrayv14@gmail.com>
2022-12-24 22:23:50 +0800

    add stats link in navigation

--- src/lib/Navigation.svelte
+++ src/lib/Navigation.svelte
@@ -44,6 +44,15 @@
 				</li>
 				<li class="bg-inherit">
 					<a
+						href="/stats"
+						class="py-2 px-3 flex justify-start items-center tracking-wide bg-inherit hover:bg-violet-700 transition-colors border-b-0 border-b-slate-400  "
+					>
+						<i class="fas fa-info-circle mr-1 w-[25px] text-center" />
+						Statistik
+					</a>
+				</li>
+				<li class="bg-inherit">
+					<a
 						href="/about"
 						class="py-2 px-3 flex justify-start items-center tracking-wide bg-inherit hover:bg-violet-700 transition-colors border-b-0 border-b-slate-400  "
 					>
