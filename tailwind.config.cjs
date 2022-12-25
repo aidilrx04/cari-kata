bc825db6009fd9f3db95614b6d17719b74e25a40
aidilrx04 <mrcrayv14@gmail.com>
2022-12-25 23:49:34 +0800

    customize screen

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		screens: {
+			sm: '320px',
+			md: '425px',
+			lg: '768px',
+			xl: '1024px'
+		},
 		extend: {}
 	},
 	plugins: []
