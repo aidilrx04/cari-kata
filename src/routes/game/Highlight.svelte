c53814037c6d6de1bb3b026f79347784e90cbbf2
aidilrx04 <mrcrayv14@gmail.com>
2023-11-06 00:41:43 +0800

    feat:rework highlight from depend on global stores

--- src/routes/game/Highlight.svelte
+++ src/routes/game/Highlight.svelte
@@ -1,49 +1,71 @@
-<script lang="ts">
-	import { CELL_WIDTH } from '$lib/stores/game';
-	import { calcDistance, calculateHighlightWidth } from '$lib/util';
-	import { onDestroy } from 'svelte';
-
-	export let highlight: { start: number[]; end: number[]; rotation: number; color: string };
-	export let mouseHighlight = false;
-
-	$: HIGHLIGHT_WIDTH = calculateHighlightWidth($CELL_WIDTH);
-
-	onDestroy(() => {
-		HIGHLIGHT_WIDTH = 0;
-	});
+<script lang="ts" context="module">
+	export const calculateDistance = (start: Coord, end: Coord) => {
+		const xLength = end.x - start.x;
+		const yLength = end.y - start.y;
+		const hipotenus = Math.sqrt(Math.pow(xLength, 2) + Math.pow(yLength, 2));
+		return hipotenus;
+	};
 </script>
 
-<!-- svelte-ignore missing-declaration -->
+<script lang="ts">
+	import type { Coord } from '$lib/types';
+	import { cellHeight, cellWidth } from './CellsManager.svelte';
+
+	// angle of rotation
+	export let angle: number;
+	export let start: Coord;
+	export let end: Coord;
+	export let color: string = '#8b5cf6';
+
+	// overwrite the default calculateHighlightWidth result if provided
+	export let width: number | undefined = undefined;
+
+	// absolute width to use either default or overwrited one
+	$: absoluteWidth = width !== undefined ? width : calculateHighlightWidth(start, end, $cellWidth);
+
+	let position: Coord; // highlight absolute position
+	// $: position = calculateHighlightPosition(start, $cellWidth);
+	$: if (start && $cellWidth) {
+		position = calculateHighlightPosition(start, $cellWidth);
+	}
+
+	$: height = ($cellHeight * 80) / 100;
+	$: halfHeight = height / 2;
+
+	// calculate the position based on coord and cell width
+	const calculateHighlightPosition = (start: Coord, width: number): Coord => {
+		const left = start.x * width + width / 2;
+		const top = start.y * width + width / 2;
+
+		return {
+			x: left,
+			y: top
+		};
+	};
+
+	export const calculateHighlightWidth = (start: Coord, end: Coord, cellWidth: number) => {
+		// calculate the distance from the center of the start coord to the center of end coord
+		const scaledStartCoord = {
+			x: start.x * cellWidth + cellWidth / 2,
+			y: start.y * cellWidth + cellWidth / 2
+		};
+		const scaledEndCoord = {
+			x: end.x * cellWidth + cellWidth / 2,
+			y: end.y * cellWidth + cellWidth / 2
+		};
+		const distance = calculateDistance(scaledStartCoord, scaledEndCoord);
+
+		return distance;
+	};
+</script>
+
 <div
-	class="current highlight"
-	style="
-				position: absolute;
-				width: 10px;
-				height: 10px;
-				opacity: 1;
-				z-index:1;
-				"
-	style:background-color={highlight.color}
-	style:height="{HIGHLIGHT_WIDTH}px"
-	style:border-radius="{HIGHLIGHT_WIDTH / 2}px {HIGHLIGHT_WIDTH / 2}px"
-	style:width="{calcDistance(
-		{
-			x:
-				highlight.start[0] * $CELL_WIDTH +
-				$CELL_WIDTH / 2 +
-				(mouseHighlight ? document.documentElement.scrollLeft : 0),
-			y:
-				highlight.start[1] * $CELL_WIDTH +
-				$CELL_WIDTH / 2 +
-				(mouseHighlight ? document.documentElement.scrollTop : 0)
-		},
-		{
-			x: !mouseHighlight ? highlight.end[0] * $CELL_WIDTH + $CELL_WIDTH / 2 : highlight.end[0],
-			y: !mouseHighlight ? highlight.end[1] * $CELL_WIDTH + $CELL_WIDTH / 2 : highlight.end[1]
-		}
-	) + HIGHLIGHT_WIDTH}px"
-	style:transform-origin="{HIGHLIGHT_WIDTH / 2}px 50%"
-	style:top="{highlight.start[1] * $CELL_WIDTH + $CELL_WIDTH / 2}px"
-	style:left="{highlight.start[0] * $CELL_WIDTH + $CELL_WIDTH / 2}px"
-	style:transform="translate(-{HIGHLIGHT_WIDTH / 2}px, -{HIGHLIGHT_WIDTH / 2}px) rotate({highlight.rotation}deg)"
+	class="w-full absolute rounded-full"
+	style:background-color={color}
+	style:width="{absoluteWidth + height}px"
+	style:height="{height}px"
+	style:top="{position.y}px"
+	style:left="{position.x}px"
+	style:transform="translate({-halfHeight}px, {-halfHeight}px) rotate({angle}deg)"
+	style:transform-origin="{halfHeight}px 50%"
 />
