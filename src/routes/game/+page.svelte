5cc60e7727cb934d42c45e7c0922fe748768303e
aidilrx04 <mrcrayv14@gmail.com>
2023-11-06 00:55:47 +0800

    feat: partial rework of main game page

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -1,173 +1,91 @@
 <script lang="ts">
-	import Modal from '$lib/Modal.svelte';
-	import Timer from '$lib/Timer.svelte';
-	import type { PageData } from './$types';
 	import Grid from './Grid.svelte';
+	import Timer from '$lib/Timer.svelte';
 	import Words from './Words.svelte';
+	import Modal from '$lib/Modal.svelte';
 	import FinishModal from './FinishModal.svelte';
-	import Meta from '$lib/Meta.svelte';
-	import Ogp from '$lib/OGP.svelte';
-	import stats from '$lib/stores/stats';
-	import {
-		game,
-		mode,
-		hideFiller,
-		checkGameFinish,
-		words,
-		CELL_WIDTH,
-		foundWords,
-		currentColor
-	} from '$lib/stores/game';
-	import { onDestroy } from 'svelte';
-	import { colors } from '$lib/util';
-	import WinSfx from '$lib/audio/win.mp3';
-	import BackgroundMusic from '$lib/audio/background.mp3';
+	import type { Solved } from '$lib/types';
 
-	export let data: PageData;
-	let showModal = true;
-	const gameInfo = {
-		grid: data.type.grid
+	let hasStarted = false;
+	let hasFinished = false;
+
+	const raw = [
+		{ id: 12563, word: 'langkat', length: 7 },
+		{ id: 1546, word: 'bacak', length: 5 }
+	];
+
+	$: words = raw.map((word) => word.word);
+
+	let wordsInGrid: string[] = []; // placed words in grid
+	let solvedWords: Solved[] = [];
+
+	let gridDimension = {
+		row: 15,
+		col: 15
 	};
 
-	$mode = data.type;
+	let showFinishedModal = false;
 
-	let winAudio: HTMLAudioElement;
-	let backgroundAudio: HTMLAudioElement;
+	let timeStartedAt: Date;
+	let timeFinishedAt: Date;
 
-	// stats
-	$: if ($game.hasStarted && $mode) {
-		const _type = $mode.type;
-		$stats = {
-			...$stats,
-			total: $stats.total + 1,
-			mode: {
-				...$stats.mode,
-				[_type]: {
-					...$stats.mode[_type],
-					total: $stats.mode[_type].total + 1
-				}
-			}
-		};
+	// check if user solved all the word
+	// and valid grid that has more than or equal to 1 word
+	$: if (wordsInGrid.length > 0 && wordsInGrid.length === solvedWords.length) {
+		if (wordsInGrid.length === solvedWords.length) stopGame();
 	}
 
-	$: if ($game.hasEnded && $mode && $game.finishTime && $game.startTime) {
-		const _type = $mode.type;
-		$stats = {
-			...$stats,
-			win: $stats.win + 1,
-			total_time: $stats.total_time + ($game.finishTime?.getTime() - $game.startTime?.getTime()),
-			mode: {
-				...$stats.mode,
-				[_type]: {
-					...$stats.mode[_type],
-					win: $stats.mode[_type].win + 1
-				}
-			}
-		};
-	}
+	const startGame = () => {
+		hasStarted = true;
+		timeStartedAt = new Date();
+	};
 
-	// check is game is finished
-	$: $checkGameFinish;
+	const stopGame = () => {
+		hasFinished = true;
+		setShowFinishedModal(true);
+	};
 
-	// $: if ($game.hasStarted) {
-	// 	backgroundAudio.loop = true;
-	// 	backgroundAudio?.play();
-	// }
-
-	$: if ($game.hasEnded) {
-		winAudio?.play();
-	}
-
-	onDestroy(() => {
-		showModal = true;
-		$game = {
-			hasEnded: false,
-			hasStarted: false,
-			startTime: null,
-			finishTime: null
-		};
-		$hideFiller = false;
-		$words = [];
-		$CELL_WIDTH = 0;
-		$foundWords = [];
-		$currentColor = colors[Math.floor(Math.random() * colors.length)];
-
-		winAudio?.pause();
-		backgroundAudio?.pause();
-	});
+	const setShowFinishedModal = (status: boolean) => {
+		showFinishedModal = status;
+	};
 </script>
 
-<Meta
-	title="Tahap {data.type.title} | Cari Kata"
-	description="Main {data.words.map((n) => 'kata ' + n.word)}"
-/>
-<Ogp title="Tahap {data.type.title} | Cari Kata" />
-
-<audio src={WinSfx} class="absolute -left-100 -top-100 win" bind:this={winAudio} />
-<audio
-	src={BackgroundMusic}
-	class="background absolute -left-100 -top-100"
-	bind:this={backgroundAudio}
-/>
-
-<main class="p-3 relative my-5">
-	{#if !$game.hasStarted}
+<main class="p-3 relative my-5 min-h-[400px]">
+	{#if !hasStarted}
 		<Modal
-			_class="bg-slate-50 dark:bg-slate-700 w-[250px] max-w-full px-2 py-1 shadow top-[70%] rounded"
+			class="bg-slate-50 dark:bg-slate-700 w-[250px] max-w-full px-2 py-1 shadow top-[70%] rounded"
 		>
 			<div slot="header" class="text-xl text-center mb-2">Cari Kata</div>
 			<div class="content">
 				<button
-					on:click={() => ($game.hasStarted = true)}
 					class="px-6 py-2 block mx-auto bg-violet-600 text-gray-50 text-xl mt-2 mb-3 rounded shadow-sm hover:bg-violet-700 transition-colors"
+					on:click={startGame}
 				>
-					Main
+					Mula
 				</button>
 			</div>
 			<div slot="footer" />
 		</Modal>
 	{/if}
-	{#if $game.hasEnded}
-		<FinishModal
-			bind:showModal
-			on:close={() => {
-				showModal = false;
-			}}
-		/>
-	{/if}
-	<div
-		class="
-default: game grid grid-cols-1 relative min-h-[400px]
-sma: mt-28
-med: md:
-lar: lg:grid-cols-2 lg:gap-3 lg:mt-0
-	"
-	>
-		{#if $game.hasStarted}
-			<Grid
-				words={data.type.preprocess ? data.type.preprocess(data.words) : data.words}
-				type={data.type}
-			/>
+	{#if hasStarted}
+		<div id="ck-game" class="grid grid-cols-2 gap-3 mt28">
+			<Grid {words} options={gridDimension} bind:wordsInGrid bind:solvedWords />
 			<div class="bg-slate-50 dark:bg-slate-700 rounded-md">
-				<header class="absolute bottom-[104%] left-0 w-full lg:static">
-					<div class="content-header flex items-center justify-between my-4 px-3">
+				<header>
+					<div class="content-header flex items-center justify-between my-4 px-4">
 						<div class="title">
-							<h2 class="text-2xl text-slate-800 dark:text-slate-100 font-semibold">
-								{data.type.title}
-							</h2>
+							<h2 class="text-2xl text-slate-800 dark:text-slate-100 font-semibold">TITLE</h2>
 							<small
 								class="text-xs font-semibold text-slate-600 dark:text-slate-300 tracking-wider"
 							>
-								{gameInfo.grid.column}x{gameInfo.grid.row} GRID
+								15 x 15 GRID
 							</small>
 						</div>
-						{#if $game.hasEnded}
-							<div>
+						{#if hasFinished}
+							<div class="finish-button">
 								<button
+									on:click={() => setShowFinishedModal(true)}
 									class="block m-1 py-2 px-3 text-gray-50 bg-violet-600 rounded uppercase hover:bg-violet-700 transition-colors"
-									on:click={() => {
-										showModal = true;
-									}}
 								>
 									SELESAI
 								</button>
@@ -177,25 +95,26 @@ lar: lg:grid-cols-2 lg:gap-3 lg:mt-0
 							<small class="label text-xs font-bold text-slate-600 dark:text-slate-300 uppercase">
 								MASA
 							</small>
-							{#if $game && $game.startTime}
-								<Timer
-									startAt={$game.startTime}
-									finishAt={$game.finishTime}
-									startCounting={$game.hasStarted && !$game.hasEnded}
-									_class="block text-slate-800 dark:text-slate-100 text-3xl"
-								/>
-							{:else}
-								<span class="placeholder block text-slate-800 dark:text-slate-100 text-3xl"
-									>00:00</span
-								>
-							{/if}
+							<Timer
+								startAt={timeStartedAt}
+								bind:finishAt={timeFinishedAt}
+								start={true}
+								stop={hasFinished}
+							/>
 						</div>
 					</div>
 				</header>
 				<div class="px-3">
-					<Words />
+					<Words {solvedWords} words={wordsInGrid} />
 				</div>
 			</div>
-		{/if}
-	</div>
+		</div>
+	{/if}
+
+	{#if hasFinished && timeStartedAt && timeFinishedAt}
+		<FinishModal
+			bind:showModal={showFinishedModal}
+			duration={timeFinishedAt.getTime() - timeStartedAt.getTime()}
+		/>
+	{/if}
 </main>
