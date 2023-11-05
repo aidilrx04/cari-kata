88a990d76dd6ea8ff88b56524d4febc28fdbf8bc
aidilrx04 <mrcrayv14@gmail.com>
2023-11-06 00:54:31 +0800

    feat: partial rework finishmodal
    - accept duration instead of accessing global stores

--- src/routes/game/FinishModal.svelte
+++ src/routes/game/FinishModal.svelte
@@ -1,24 +1,16 @@
 <script lang="ts">
-	import { game, mode } from '$lib/stores/game';
 	import Modal from '$lib/Modal.svelte';
-	import { MODE_TYPES } from '$lib/modes';
 	import { onDestroy } from 'svelte';
 
 	export let showModal = true;
+	export let duration: number;
 
 	onDestroy(() => {
 		showModal = false;
 	});
 
-	const colors = ['text-cyan-500', 'text-blue-500', 'text-red-500', 'text-rose-700'];
-	const modeColors = Object.entries(MODE_TYPES).reduce(
-		(acc, mode, i) => ({ ...acc, [mode[0]]: colors[i] }),
-		{}
-	);
-
-	const distance = ($game.finishTime as Date).getTime() - ($game.startTime as Date).getTime();
-	const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	const minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
+	const seconds = Math.floor((duration % (1000 * 60)) / 1000);
 </script>
 
 <div class="finish-modal">
@@ -27,7 +19,7 @@
 			on:close={() => {
 				showModal = false;
 			}}
-			_class="bg-slate-50 dark:bg-slate-600 shadow-lg p-3 rounded-lg"
+			class="bg-slate-50 dark:bg-slate-600 shadow-lg p-3 rounded-lg"
 			transitionProps={{
 				out: {
 					duration: 0
@@ -41,7 +33,7 @@
 				<p class="finish-message text-slate-800 dark:text-slate-200 text-center text-md">
 					Anda telah menyelesaikan cabaran
 					<br />
-					<span class="{modeColors[$mode.type]} text-xl uppercase">{$mode.title}</span>
+					<span class="text-xl uppercase">Cari Kata</span>
 					<br />
 					dalam masa
 					<br />
