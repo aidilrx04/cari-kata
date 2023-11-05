85f0536cd1435620e9c4b6f96baa5c8a3cea6ff2
aidilrx04 <mrcrayv14@gmail.com>
2023-11-06 00:49:33 +0800

    feat: rework timer
    - show hours if time is longer
    - make time stop controlable from outside

--- src/lib/Timer.svelte
+++ src/lib/Timer.svelte
@@ -4,37 +4,47 @@
 
 	export let startAt: Date;
 	export let finishAt: Date | null = null;
-	export let startCounting: boolean = false;
-	export let _class: string = '';
+	export let start: boolean = false;
+	export let stop = false;
 
 	const startAtTime = startAt.getTime();
+	let hours = 0;
 	let minutes = 0;
 	let seconds = 0;
 
-	let intervalId;
-	$: if (startCounting) {
-		intervalId = setInterval(() => {
+	let timerInterval;
+	$: if (start) {
+		timerInterval = setInterval(() => {
 			const now = new Date().getTime();
 
 			const distance = now - startAtTime;
 			// console.log(distance);
 
-			// find minutes and seconds
+			// find hours, minutes and seconds
+			hours = Math.floor(distance / (1000 * 60 * 60));
 			minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 			seconds = Math.floor((distance % (1000 * 60)) / 1000);
 		}, 1000);
-	} else {
-		if (!finishAt) {
-			finishAt = new Date();
-		}
-		clearInterval(intervalId);
+	}
+
+	// only stop if timer has started, else is invalid
+	$: if (stop && start) {
+		finishAt = new Date();
+		clearInterval(timerInterval);
 	}
 
 	onDestroy(() => {
-		clearInterval(intervalId);
+		clearInterval(timerInterval);
 	});
 </script>
 
-<div class="timer {_class}">
-	{addPrefix(minutes, '0', 2)}:{addPrefix(seconds, '0', 2)}
+<div class="timer flex text-slate-800 dark:text-slate-100 text-3xl">
+	{#if hours > 0}
+		<!-- only show hours if minutes > 60 -->
+		<span>{addPrefix(hours, '0', 2)}</span>
+		<span>:</span>
+	{/if}
+	<span>{addPrefix(minutes, '0', 2)}</span>
+	<span>:</span>
+	<span>{addPrefix(seconds, '0', 2)}</span>
 </div>
