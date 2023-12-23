b753764243b1a3d45447fbc7cb20a444df71b138
aidilrx04 <mrcrayv14@gmail.com>
2023-12-23 10:22:47 +0800

    feat: add register page

---
+++ src/routes/auth/register/+page.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+	import { getToken } from '$lib/authtoken';
+	import { API_URL } from '$lib/config';
+	import Cookies from 'js-cookie';
+
+	let userData = {
+		username: '',
+		password: ''
+	};
+
+	const handleSubmit = async () => {
+		const formBody = JSON.stringify(userData);
+
+		const raw = await fetch(`${API_URL}/auth/register`, {
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			},
+			method: 'post',
+			body: formBody
+		});
+
+		const response = await raw.json();
+
+		// store user token into cookie
+		Cookies.set('token', response.token, {
+			path: '/'
+		});
+	};
+</script>
+
+<div class="md:max-w-screen-md mx-auto">
+	<header class="my-3">
+		<h1 class="text-2xl text-slate-800 dark:text-slate-50 uppercase tracking-wider">Cipta Akaun</h1>
+	</header>
+	<main class="p-3 bg-slate-50 dark:bg-slate-700 rounded-lg max-w-full">
+		<form on:submit|preventDefault={handleSubmit}>
+			<label for="username" class="input-group">
+				<span>Nama</span>
+				<input
+					bind:value={userData.username}
+					class="block w-full rounded bg-slate-50 dark:bg-slate-800 border border-slate-200 dark:border-slate-600 py-3 px-2"
+					type="text"
+					id="username"
+				/>
+			</label>
+			<label for="password" class="input-group">
+				<span>Password</span>
+				<input
+					bind:value={userData.password}
+					class="block w-full rounded bg-slate-50 dark:bg-slate-800 border border-slate-200 dark:border-slate-600 py-3 px-2"
+					type="password"
+					id="password"
+				/>
+			</label>
+			<div class="input-group">
+				<button
+					type="submit"
+					class="block w-full bg-violet-600 px-4 py-3 rounded text-slate-50 hover:bg-violet-700 active:focus:bg-violet-800"
+				>
+					Register
+				</button>
+			</div>
+		</form>
+
+		<style>
+			.input-group {
+				@apply mb-4 block;
+			}
+
+			.input-group span {
+				@apply block mb-2;
+			}
+		</style>
+	</main>
+</div>
