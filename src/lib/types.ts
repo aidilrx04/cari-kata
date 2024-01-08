6c75cea8723393ccb6cace977ea6b0d2df966f4d
aidilrx04 <mrcrayv14@gmail.com>
2024-01-08 23:35:21 +0800

    feat: add cell/highlight movement cb functions

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,5 @@
+import type { HighlightData } from '../routes/game/HighlightManager.svelte';
+
 export type Word = {
 	value: string;
 	display: string;
@@ -57,3 +59,43 @@ export type GridOptions = {
 };
 
 export type DisplayWordFunction = (word: Word, solvedWords: Solved[]) => string;
+
+export type CellUpdateFunction = (cell: { value: string }) => void;
+
+export type OnCellPressFunction = (
+	cell: {
+		coord: Coord;
+		update: CellUpdateFunction;
+	},
+	grid: {
+		grid: string[][];
+	}
+) => void;
+
+export type OnCellReleaseFunction = (
+	cell: {
+		coord: Coord;
+	},
+	grid: {
+		grid: string[][];
+		updateGrid: (grid: string[][]) => void;
+	}
+) => void;
+
+export type HighlightUpdate<T> = (data: Partial<T>) => void;
+export type CalcHighlightWidth = (start: Coord, end: Coord) => number;
+
+export type OnCellMoveFunction<T = object> = (
+	start: Coord,
+	current: Coord | null,
+	highlight: T & {
+		update: HighlightUpdate<T>;
+		calcHighlightWidth: CalcHighlightWidth;
+	},
+	options: {
+		highlights: HighlightData[];
+		addHighlight: (data: Omit<HighlightData, 'id'>) => HighlightData;
+		updateHighlight: (id: number, data: Partial<Omit<HighlightData, 'id'>>) => void;
+		removeHighlight: (id: number) => void;
+	}
+) => void;
