45c9c235b5fe2fb428b1ae7508cf571b556c96c4
aidilrx04 <mrcrayv14@gmail.com>
2024-02-08 22:47:52 +0800

    feat: add basic create game

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import type { Game, OnSuccessPlacement, WordInGrid } from '$lib/types';
+	import ConfirmCreateTab from './ConfirmCreateTab.svelte';
 	import CreateGrid from './CreateGrid.svelte';
 	import CreateWords from './CreateWords.svelte';
 
@@ -25,6 +26,8 @@
 	let totalWords = 9;
 	let words: string[] = ['hei', 'whatsup', 'gais', 'harini', 'kita', 'makan', 'sayur', 'berapi'];
 	let selected: string = '';
+
+	let currTab = 2;
 
 	const handleSubmit = () => {
 		fetch('/create', {
@@ -66,72 +69,87 @@
 		];
 		selected = '';
 	};
+
+	const changeTab = (tab: number) => {
+		currTab = tab;
+	};
+
+	const nextTab = () => {
+		changeTab(currTab + 1);
+	};
+
+	const prevTab = () => {
+		changeTab(currTab - 1);
+	};
 </script>
 
 <header>
 	<h1 class="uppercase text-2xl text-center mt-6 mb-3 tracking-wider">Cipta permainan</h1>
 </header>
 
-<main class="p-3 max-w-full bg-slate-50 dark:bg-slate-700 rounded-lg">
-	<form on:submit={handleSubmit}>
-		<label class="input-group" for="title">
-			<span>Tajuk</span>
-			<input bind:value={game.title} type="text" id="title" placeholder="Title" />
-		</label>
-		<label for="words" class="input-group">
-			<span>Jumlah perkataan</span>
-			<input bind:value={totalWords} type="number" id="words" />
-		</label>
+<main id="create-game" class="p-3 max-w-full bg-slate-50 dark:bg-slate-700 rounded-lg">
+	<form on:submit={handleSubmit} class="section-container w-full">
+		<div id="section1" class="section">
+			{#if currTab === 0}
+				<div class="w-[360px] max-w-full mx-auto">
+					<label class="input-group" for="title">
+						<span>Tajuk</span>
+						<input bind:value={game.title} type="text" id="title" placeholder="Title" />
+					</label>
+					<div class="input-group mt-6">
+						<button
+							on:click={() => {
+								changeTab(1);
+							}}
+							type="button"
+							class="next nav-btn">Next</button
+						>
+					</div>
+				</div>
+			{:else if currTab === 1}
+				<style>
+					main#create-game {
+						@apply bg-transparent;
+					}
+				</style>
+				<h2 class="mb-5 text-xl text-center">Game Generation</h2>
 
-		<div class="relative my-4 text-center">
-			<hr class="z-10 absolute top-1/2 left-0 w-full" />
-			<span class="relative px-4 bg-slate-50 z-20">Grid</span>
-		</div>
-
-		<div class="flex w-full gap-5">
-			<label for="rows" class="input-group w-full">
-				<span>Baris</span>
-				<input bind:value={gridData.rows} type="number" id="rows" />
-			</label>
-			<label for="columns" class="input-group w-full">
-				<span>Lajur</span>
-				<input bind:value={gridData.columns} type="number" id="columns" />
-			</label>
-		</div>
-
-		<label for="grid-generation" class="input-group">
-			<span>Generasi</span>
-			<select id="grid-generation">
-				<option value="automatic">Automatik</option>
-				<option value="manual">Manual</option>
-			</select>
-		</label>
-
-		<div class="flex">
-			<CreateWords bind:words bind:selected bind:placedWords />
-			<CreateGrid
-				bind:rows={gridData.rows}
-				bind:columns={gridData.columns}
-				bind:grid
-				bind:solved
-				bind:selected
-				bind:placedWords
-				onSuccessPlacement={handleSuccessPlacement}
-			/>
-		</div>
-
-		<div class="input-group">
-			<button
-				type="submit"
-				class="block w-full py-3 px-2 rounded text-slate-50 bg-violet-600 hover:bg-violet-700 active:focus:bg-violet-800"
-			>
-				Cipta
-			</button>
+				<div class="">
+					<CreateWords bind:words bind:selected bind:placedWords {nextTab} />
+					<CreateGrid
+						bind:rows={gridData.rows}
+						bind:columns={gridData.columns}
+						bind:grid
+						bind:solved
+						bind:selected
+						bind:placedWords
+						onSuccessPlacement={handleSuccessPlacement}
+					/>
+				</div>
+				<!-- <div class="input-group !flex gap-2">
+					<button
+						on:click={() => {
+							changeTab(0);
+						}}
+						type="button"
+						class="prev nav-btn">Back</button
+					>
+					<button
+						on:click={() => {
+							changeTab(2);
+						}}
+						type="button"
+						class="next nav-btn">Next</button
+					>
+				</div> -->
+			{:else if currTab === 2}
+				<ConfirmCreateTab title={game.title} words={placedWords.map((w) => w.value)} {prevTab} />
+			{/if}
 		</div>
 	</form>
 </main>
 
-<style>
+<style lang="scss">
 	.input-group {
 		@apply block mb-4;
 	}
@@ -143,5 +161,42 @@
 	.input-group input,
 	.input-group select {
 		@apply block w-full py-3 px-2 bg-slate-50 rounded border  border-slate-200;
+	}
+
+	.section-container {
+		.section {
+			@apply min-w-full;
+
+			:global(.nav-btn) {
+				@apply block py-2 px-3 text-center w-full rounded;
+
+				&.next {
+					@apply bg-violet-600 text-slate-50;
+
+					&:hover {
+						@apply bg-violet-700;
+					}
+				}
+				&.prev {
+					@apply text-slate-600 bg-slate-100;
+
+					&:hover {
+						@apply bg-slate-200;
+					}
+				}
+			}
+
+			.radio-input {
+				@apply flex items-center justify-start gap-2;
+
+				[type='radio'] {
+					@apply w-auto;
+				}
+
+				span {
+					@apply mb-0;
+				}
+			}
+		}
 	}
 </style>
