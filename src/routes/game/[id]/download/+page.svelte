d46ba4751a9ab5066c4db330bf7db9b2f0828d95
aidilrx04 <mrcrayv14@gmail.com>
2024-02-12 22:33:01 +0800

    feat: add download page skeleto n

---
+++ src/routes/game/[id]/download/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+</script>
+
+<main>
+	<h1>Download [Game Title]</h1>
+
+	<section>
+		<h2>Word Position</h2>
+		<div class="input-group">
+			<input type="radio" id="pos-top" value="top" />
+			<label for="pos-top">Top</label>
+		</div>
+		<div class="input-group">
+			<input type="radio" id="pos-left" value="left" />
+			<label for="pos-left">Left</label>
+		</div>
+		<div class="input-group">
+			<input type="radio" id="pos-right" value="right" />
+			<label for="pos-right">Right</label>
+		</div>
+		<div class="input-group">
+			<input type="radio" id="pos-bottom" value="bottom" />
+			<label for="pos-bottom">Top</label>
+		</div>
+	</section>
+
+	<section>
+		<h2>Preview</h2>
+
+		<!-- <PreviewGrid /> -->
+	</section>
+</main>
